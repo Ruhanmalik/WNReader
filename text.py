d8860c230ea04d606a7f29589331f539f0209fcc
Ruhan Malik <ruhanmalik@Ruhans-Air.attlocal.net>
2025-07-03 23:35:25 -0500

    add6

--- text.py
+++ text.py
@@ -1,1 +1,8 @@
-text = input("Enter the text you want generated into an audiofile: ")
+from TTS.api import TTS
+tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2", gpu=False)
+
+# generate speech by cloning a voice using default settings
+tts.tts_to_file(text="It took me quite a long time to develop a voice, and now that I have it I'm not going to be silent.",
+                file_path="output.wav",
+                speaker_wav="/path/to/target/speaker.wav",
+                language="en")
